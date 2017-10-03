8f694dda1e7998368da0b8459f0b726b3aa73ff7
Pieter Algera <pieter@joyjet.com>
2017-10-03 15:36:28 +0200

    Removed Clarifai which should remove the error

--- quickstart.py
+++ quickstart.py
@@ -12,32 +12,33 @@ session.login()
 
 # set up all the setting
 session.set_do_comment(False, percentage=0)
-
+session.set_use_clarifai(enabled=False)
 # do the actual liking
-#Strategy 1: Hashtab
-session.like_by_tags(['entrepreneur'], amount=randrange(30, 70))
-sleep(3600)
-session.like_by_tags(['entrepreneurlife'], amount=randrange(30, 70))
-sleep(3600)
-session.like_by_tags(['digitalagency'], amount=randrange(30, 70))
-sleep(3600)
-session.like_by_tags(['agencyowner'], amount=randrange(30, 70))
-sleep(3600)
-session.like_by_tags(['entrepreneursofinstagram'], amount=randrange(30, 70))
-sleep(3600)
+while True:
+    #Strategy 1: Hashtab
+    session.like_by_tags(['entrepreneur'], amount=randrange(30, 70))
+    sleep(3600)
+    session.like_by_tags(['entrepreneurlife'], amount=randrange(30, 70))
+    sleep(3600)
+    session.like_by_tags(['digitalagency'], amount=randrange(30, 70))
+    sleep(3600)
+    session.like_by_tags(['agencyowner'], amount=randrange(30, 70))
+    sleep(3600)
+    session.like_by_tags(['entrepreneursofinstagram'], amount=randrange(30, 70))
+    sleep(3600)
 
-#strategy 2: Follower liking
-session.set_user_interact(amount=10, random=True, percentage=100, media='Photo')
-session.set_do_like(enabled=True, percentage=100)
-session.set_do_follow(enabled=True, percentage=100)
-session.interact_user_followers(['ginlane'], amount=randrange(10, 40), random=True)
-sleep(3600)
-session.interact_user_followers(['ryanserhant'], amount=randrange(10, 40), random=True)
-sleep(3600)
-session.interact_user_followers(['Fueled'], amount=randrange(10, 40), random=True)
-sleep(3600)
-session.interact_user_followers(['garyvee'], amount=randrange(10, 40), random=True)
-sleep(3600)
+    #strategy 2: Follower liking
+    session.set_user_interact(amount=10, random=True, percentage=100, media='Photo')
+    session.set_do_like(enabled=True, percentage=100)
+    session.set_do_follow(enabled=True, percentage=100)
+    session.interact_user_followers(['ginlane'], amount=randrange(10, 40), random=True)
+    sleep(3600)
+    session.interact_user_followers(['ryanserhant'], amount=randrange(10, 40), random=True)
+    sleep(3600)
+    session.interact_user_followers(['Fueled'], amount=randrange(10, 40), random=True)
+    sleep(3600)
+    session.interact_user_followers(['garyvee'], amount=randrange(10, 40), random=True)
+    sleep(3600)
 
 # end the bot session
 session.end()
