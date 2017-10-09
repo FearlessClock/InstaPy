c1199a8af04c756ad22279970d821c929ca61d31
Pieter Algera <pieter@joyjet.com>
2017-10-09 09:34:06 +0200

    Added new login and tag/user info

--- quickstart.py
+++ quickstart.py
@@ -2,8 +2,8 @@ from instapy import InstaPy
 from random import randrange
 from time import sleep 
 
-insta_username = 'jattecizexi9495'
-insta_password = '12345Azerty'
+insta_username = 'pierothepup'
+insta_password = 'spooky12'
 
 # if you want to run this script on a server, 
 # simply add nogui=True to the InstaPy() constructor
@@ -16,29 +16,21 @@ session.set_use_clarifai(enabled=False)
 # do the actual liking
 while True:
     #Strategy 1: Hashtab
-    session.like_by_tags(['entrepreneur'], amount=randrange(30, 70))
-    sleep(3600)
-    session.like_by_tags(['entrepreneurlife'], amount=randrange(30, 70))
-    sleep(3600)
-    session.like_by_tags(['digitalagency'], amount=randrange(30, 70))
-    sleep(3600)
-    session.like_by_tags(['agencyowner'], amount=randrange(30, 70))
-    sleep(3600)
-    session.like_by_tags(['entrepreneursofinstagram'], amount=randrange(30, 70))
-    sleep(3600)
-
+    tagsList = ['bluemerle', 'bluemerlepom', 'bluemerlepomeranian', 'dogstagram', 'dogoftheday', 'pupsofinstagram', 'pup', '6weeksold', 'sleepypuppy', 'pominu', 'puppyboots', 'boots', 'spots', 'bluemerlepom', 'bluemerle', 'pomeranianshibainu', 'shibainu', 'pomeranianpuppy'\
+            'sleepingpuppy', 'puppybaby', 'doggystyle', 'thedogslife', 'thedoggycalendar', 'littlebabe', 'littledog', 'designerdog', 'mix', 'shibainupuppy', 'shibainu', 'pominu', 'pom', 'pomeranianpuppy', 'pomeranian', 'puppiesofig']
+    for i in range(len(tagsList)):
+        session.like_by_tags([tagsList[i]], amount=randrange(30, 70))
+        sleep(3600)
+    
     #strategy 2: Follower liking
     session.set_user_interact(amount=10, random=True, percentage=100, media='Photo')
     session.set_do_like(enabled=True, percentage=100)
     session.set_do_follow(enabled=True, percentage=100)
-    session.interact_user_followers(['ginlane'], amount=randrange(10, 40), random=True)
-    sleep(3600)
-    session.interact_user_followers(['ryanserhant'], amount=randrange(10, 40), random=True)
-    sleep(3600)
-    session.interact_user_followers(['Fueled'], amount=randrange(10, 40), random=True)
-    sleep(3600)
-    session.interact_user_followers(['garyvee'], amount=randrange(10, 40), random=True)
-    sleep(3600)
+
+    userList = ['jiffpom', 'pomeranianworld', 'mr.monsterpup', 'thedogist', 'dogsofinstagram']
+    for i in range(len(userList)):
+        session.interact_user_followers([userList[i]], amount=randrange(10, 40), random=True)
+        sleep(3600)
 
 # end the bot session
 session.end()
